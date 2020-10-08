5bcc3b6ed342c1355c8bf0100e8524662e524be5
abhivikrant <vikrantabhishek72@gmail.com>
2020-10-08 11:16:51 +0530

    Created components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,42 @@
 import React from "react";
-
+import Main from "./Main";
+import Sidebar from "./Sidebar";
 const App = () => {
-  return <div>React-Router</div>;
+  const users = [
+    {
+      fullName: "Abhishek",
+      email: "abhi@word.com",
+      gender: "male"
+    },
+    {
+      fullName: "Praveen",
+      email: "praveen@word.com",
+      gender: "male"
+    },
+    {
+      fullName: "Santosh",
+      email: "santosh@word.com",
+      gender: "male"
+    },
+    {
+      fullName: "Ruchi",
+      email: "ruchi@word.com",
+      gender: "female"
+    },
+    {
+      fullName: "Princy",
+      email: "princy@word.com",
+      gender: "female"
+    }
+  ];
+
+  return (
+    <div>
+      React-Router
+      <Sidebar />
+      <Main />
+    </div>
+  );
 };
 
 export default App;
